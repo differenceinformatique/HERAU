27b1a41fb3b8133943e14ee6e1208132fe0f9969
Steven CASTEL <steven.castel@groupeadinfo.com>
2018-01-25 14:53:17 +0100

    unicite_cond_piece

--- addons_gesprim/difodoo_fichiers_base/models/di_inherited_product.py
+++ addons_gesprim/difodoo_fichiers_base/models/di_inherited_product.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
+from odoo import osv
+from odoo.exceptions import Warning
 from odoo import models, fields, api
 from xlrd.formula import FMLA_TYPE_COND_FMT
 
 class DiInheritedProduct(models.Model):
     _inherit = "product.template"
-    
-    
-    #di_default_code_req = fields.Char('Code article', store=False)
-#     
+      
     di_lavage = fields.Boolean(string="Lavage", default=False)
     di_prixmin = fields.Float(string="Prix minimum")
     di_prixmax = fields.Float(string="Prix maximum")
@@ -25,9 +24,6 @@ class DiInheritedProduct(models.Model):
     di_calibre_id = fields.Many2one("di.calibre",string="Calibre")
     di_calibre_di_des = fields.Char(related='di_calibre_id.di_des')#, store='False')
     
-#     di_emballage_id = fields.Many2one("di.emballage",string="Emballage")
-#     di_emballage_di_des = fields.Char(related='di_emballage_id.di_des')#, store='False')
-    
     di_enlevement_id = fields.Many2one("di.enlevement",string="Enlèvement")
     di_enlevement_di_des = fields.Char(related='di_enlevement_id.di_des')#, store='False')
     
@@ -39,76 +35,69 @@ class DiInheritedProduct(models.Model):
     di_type_colis       = fields.Many2one('product.packaging', string='Colis')
      
     @api.one
-#     @api.model
     def di_get_type_piece(self):
-#         if not self.id:
-#             return False
-#         ProductPackObj = self.env['product.packaging']
-#         
-#         return ProductPackObj.di_get_product_packaging(self.id,"PIECE")
         ProductPack = self.env['product.packaging'].search([
             '|',
             ('product_id', '=', self.id),
             ('di_type_cond', '=', 'PIECE')]).id
         return ProductPack
         
-
-#     @api.model
-#     def _auto_init(self):
-#         #self.di_default_code_req.required=False        
-#         super(DiInheritedProduct, self)._auto_init()
-#         # Now safely perform your own stuff
-#         self.env.cr.execute("update product_template set di_default_code_req = '3' where di_default_code_req is null;")
-#         self.di_default_code_req.required=True
-#        # super(DiInheritedProduct, self)._auto_init()
-#         
-#     @api.depends('di_default_code_req')
-#     def _compute_default_code(self):
-#       self.default_code = self.di_default_code_req
-
 class DiInheritedProductProduct(models.Model):
     _inherit = "product.product"
     
-    
     @api.multi
-#     @api.model
     def di_get_type_piece(self):
-#         if not self.id:
-#             return False
-#         ProductPackObj = self.env['product.packaging']
-#         
-#         return ProductPackObj.di_get_product_packaging(self.id,"PIECE")
         ProductPack = self.env['product.packaging'].search(['&',('product_id', '=', self.id),('di_type_cond', '=', 'PIECE')])
         return ProductPack
         
-
-
 class DiInheritedProductPackaging(models.Model):
     _inherit = "product.packaging"
     
     di_qte_cond_inf = fields.Float(string='Quantité conditionnement inférieur')
-    di_type_cond    = fields.Selection([("PIECE", "Pièce"), ("COLIS", "Colis"),("PALETTE", "Palette")], string="Type de conditionnement")
-    # TODO : n'autoriser qu'un seul type pièce
+    di_type_cond    = fields.Selection([("PIECE", "Pièce"), ("COLIS", "Colis"),("PALETTE", "Palette")], string="Type de conditionnement")    
     di_type_colis       = fields.Many2one('product.packaging', string='Colis') 
            
-#     @api.model
-#     def create(self, vals):
-#         if vals["di_type_cond"]=="PIECE":
-#             ProductPack = self.env['product.packaging'].search(['&',('product_id', '=', vals["product_id"]),('di_type_cond', '=', vals["di_type_cond"])])
-#             if ProductPack.id == False: 
-#                 rec = super(ProductPackaging, self).create(vals)            
-#             else:
-#                 rec = False
-#         else:
-#             rec = super(ProductPackaging, self).create(vals)
-#         return rec
-    #TODO : empecher la créationde plusieurs conditionnements de type pièce
+    @api.model
+    def create(self,vals):
+        #surcharge de la fonction create
+        if vals["di_type_cond"]=="PIECE": # si le type de conditionnement est PIECE
+            #recherche de l'enregistrement product.packaging avec un type de conditionnement = PIECE
+            ProductPack = self.env['product.packaging'].search(['&',('product_id', '=', vals["product_id"]),('di_type_cond', '=', vals["di_type_cond"])])
+            #Si on ne le trouve pas: OK , on créé le packaging
+            if ProductPack.id == False: 
+                rec = super(DiInheritedProductPackaging, self).create(vals)            
+            else:
+                #sinon on retourne false -> pas de création avec un message d'erreur
+                rec = False
+                raise Warning("Vous ne pouvez pas avoir plusieurs conditionnements de type Pièce pour un même article.")   
+        else:
+             rec = super(DiInheritedProductPackaging, self).create(vals)
+        return rec
     
-#     @api.onchange('di_type_cond')
-#     def di_controle_type_piece_unique(self):
-#         if self.di_type_cond =='PIECE':
-#             ProductPack = self.env['product.packaging'].search(['&',('product_id', '=', self.product_id),('di_type_cond', '=', self.di_type_cond)])
-#             if ProductPack.id != False:             
-#                 raise osv.except_osv(('Erreur'), ('Vous ne pouvez pas avoir plusieurs conditionement de type pièce.'))
-#                 self.di_type_cond = 'COLIS'
+    @api.multi
+    def write(self,vals):   
+        #surcharge de write
+        modif_type_cond = False     # initialisation d'une variable       
+        di_ctx=dict(self._context or {}) # chargement du contexte
+        for key in vals.items(): # vals est un dictionnaire qui contient les champs modifiés, on va lire les différents enregistrements
+            #on est obligé de faire cette boucle car quand on valide la fiche article, pour les conditionnements non modifiés on passe quand même ici avec
+            # seulement la key product_id donc on au une erreur sur la suite car il ne trouve pas de vals["di_type_cond"]
+            if key[0] == "di_type_cond": # si on a modifié di_type_cond
+                modif_type_cond = True
+        if modif_type_cond==True:
+            if vals["di_type_cond"]=="PIECE": # si on a bien un type PIECE
+                #recherche de l'enregistrement product.packaging avec un type de conditionnement = PIECE
+                ProductPack = self.env['product.packaging'].search(['&',('product_id', '=', self.product_id.id),('di_type_cond', '=', vals["di_type_cond"])])            
+                if ProductPack.id == False: 
+                    #Si on ne le trouve pas: OK , on créé le packaging
+                    rec = super(DiInheritedProductPackaging, self.with_context(di_ctx)).write(vals)            
+                else:
+                    rec = False
+                    #sinon on retourne false -> pas de création avec un message d'erreur
+                    raise Warning("Vous ne pouvez pas avoir plusieurs conditionnements de type Pièce pour un même article.")   
+            else:
+                 rec = super(DiInheritedProductPackaging, self.with_context(di_ctx)).write(vals)            
+        else:
+            rec = super(DiInheritedProductPackaging, self.with_context(di_ctx)).write(vals)
+        return rec    
     
