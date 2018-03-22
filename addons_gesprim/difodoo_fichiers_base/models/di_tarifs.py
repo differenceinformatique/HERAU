d4ee0e27680818cf44c8cadc7f2a7bfe5ffb43d4
Steven CASTEL <steven.castel@groupeadinfo.com>
2018-03-22 11:13:22 +0100

    wizard

---
+++ addons_gesprim/difodoo_fichiers_base/models/di_tarifs.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+    
+class DiTarifs(models.Model):
+    _name = "di.tarifs"
+    _description = "Tarifs"
+    _order = "name"
+    
+#     name = fields.Char(string="Code", required=True)
+    di_company_id = fields.Many2one('res.company', string='Société', readonly=True,  default=lambda self: self.env.user.company_id)             
+    di_product_id = fields.Many2one('product.product', string='Article', required=True)    
+    di_partner_id = fields.Many2one('res.partner',string="Client")
