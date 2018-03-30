4af64ae819c4cfdef17d2bfcca3852967f7ed87e
Steven CASTEL <steven.castel@groupeadinfo.com>
2018-03-30 12:35:58 +0200

    commentaires gen tar

--- addons_gesprim/difodoo_fichiers_base/wizard/di_generer_tarifs_wizard.py
+++ addons_gesprim/difodoo_fichiers_base/wizard/di_generer_tarifs_wizard.py
@@ -17,10 +17,12 @@ class DiGenTarWiz(models.TransientModel):
     
     @api.multi
     def generer_tarifs(self):
-        # parcours des codes tarifs  
+        # parcours des codes tarifs  destinationsélectionnés
         for tarifdest in self.di_tarifs_dest_ids:
-            if tarifdest.id != self.di_tarif_orig_id.id:
-                for tarif_origine in self.env["di.tarifs"].search(['&',('di_code_tarif_id', '=', self.di_tarif_orig_id.id),('di_date_effet','=',self.di_date_effet_orig)]):                    
+            if tarifdest.id != self.di_tarif_orig_id.id: # on vérifie que le code tarif destination n'est pas le tarif d'origine
+                for tarif_origine in self.env["di.tarifs"].search(['&',('di_code_tarif_id', '=', self.di_tarif_orig_id.id),('di_date_effet','=',self.di_date_effet_orig)]): 
+                    #parcours de tarifs origine correspondants au code tarif et à la date d'effet sélectionnée   
+                    #chargement des données du nouveau tarif                
                     data = {
                             'di_product_id': tarif_origine.di_product_id.id,
                             'di_code_tarif_id': tarifdest.id,
@@ -31,7 +33,7 @@ class DiGenTarWiz(models.TransientModel):
                             'di_date_effet': self.di_date_effet,
                             'di_date_fin': self.di_date_fin                                  
                             }                       
-                                                        
+                    #recherche si tarif existant                                    
                     tarif_existant = self.env["di.tarifs"].search(['&',('di_code_tarif_id', '=', tarifdest.id),
                                                                    ('di_date_effet','=',self.di_date_effet),
                                                                    ('di_company_id','=',self.env.user.company_id.id),
@@ -40,30 +42,33 @@ class DiGenTarWiz(models.TransientModel):
                                                                    ('di_un_prix','=',tarif_origine.di_un_prix),
                                                                    ('di_qte_seuil','=',tarif_origine.di_qte_seuil)
                                                                    ])
+                    
                     if tarif_existant:
+                        # si il existe, on le met à jour
                         tarif_existant.update(data)     
                     else:
+                        #sinon on le créé
                         self.env["di.tarifs"].create(data)                                                        
             
     @api.model
     def default_get(self, fields):
         res = super(DiGenTarWiz, self).default_get(fields) 
-        if self.env.context.get('active_model'): 
-            active_model=self.env.context['active_model'] # recup du contexte de l'act_window
+        if self.env.context.get('active_model'): # on vérifie si on est dans un model
+            active_model=self.env.context['active_model'] #récup du model courant
         else:
             active_model=''
         if active_model :    
-            if active_model == 'di.code.tarif':
+            if active_model == 'di.code.tarif': # si lancé à partir des codes tarifs
                 tarif_id = self.env.context["active_id"]
                 Tarif = self.env["di.code.tarif"].browse(tarif_id)
                 res["di_tarif_orig_id"] = Tarif.id
-            elif active_model=='di.tarifs':
+            elif active_model=='di.tarifs': # si lancé à partir des tarifs
                 tarif_id = self.env.context["active_id"]
                 Tarif = self.env["di.tarifs"].browse(tarif_id)
                 res["di_tarif_orig_id"] = Tarif.di_code_tarif_id.id                                            
                 res["di_date_effet_orig"] = Tarif.di_date_effet
                 res["di_date_effet"] = Tarif.di_date_effet
                 res["di_date_fin"] = Tarif.di_date_fin                                  
-        else:
-            res["di_date_effet_orig"] = datetime.today()                                     
+        else: # lancé à partir du menu
+            res["di_date_effet_orig"] = datetime.today()      # je charge la date à aujourd'hui car si rien n'est envoyé, le wizard ne s'ouvre pas                               
         return res    
