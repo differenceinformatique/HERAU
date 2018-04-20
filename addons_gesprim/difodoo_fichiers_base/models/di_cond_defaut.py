f275997947735ad8a67042fff348207ff1bdc755
Steven CASTEL <steven.castel@groupeadinfo.com>
2018-04-20 14:46:01 +0200

    Cond réf

--- addons_gesprim/difodoo_fichiers_base/models/di_cond_defaut.py
+++ addons_gesprim/difodoo_fichiers_base/models/di_cond_defaut.py
@@ -10,4 +10,4 @@ class DiCondDefaut(models.Model):
     di_company_id = fields.Many2one('res.company', string='Société', readonly=True,  default=lambda self: self.env.user.company_id)             
     di_des = fields.Char(string="Désignation", required=True)
     name = fields.Char(string="Code", required=True)
-    di_type_cond = fields.Selection([("PIECE", "Pièce"), ("COLIS", "Colis"),("PALETTE", "Palette")], string="Type de conditionnement")    
+    di_type_cond = fields.Selection([("PIECE", "Cond. Réf."), ("COLIS", "Colis"),("PALETTE", "Palette")], string="Type de conditionnement")    
