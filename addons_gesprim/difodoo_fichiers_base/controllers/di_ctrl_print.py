c2f9ca9b688d2b4c701a367b1bee4894094cde72
Steven CASTEL <steven.castel@groupeadinfo.com>
2018-06-20 10:28:46 +0200

    enlever_etiq_SC

--- addons_gesprim/difodoo_fichiers_base/controllers/di_ctrl_print.py
+++ addons_gesprim/difodoo_fichiers_base/controllers/di_ctrl_print.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models,fields,api
+from odoo import models,fields,api,http
 #from . import models,wizards
 import os, sys
 import win32print
+from Naked.toolshed.shell import execute_js, muterun_js
+# class MyController(http.Controller):
+#     @http.route(type='json', website=True)
+#     def imp_javascript(self,data, **kw):
+# 
+#       #Fetch input json data sent from js
+#         openWin(data)
+#        # Your code is here 
+
+
 
 def format_data(labelmodelfile,charSep,parameters):
     contenu = "";
@@ -22,13 +32,25 @@ def format_data(labelmodelfile,charSep,parameters):
     
     return contenu 
         
+        
+    
 
+      
 def printlabelonwindows(printer,contenu):
     if sys.version_info >= (3,):
         raw_data = bytes(contenu,"utf-8")
     else:
         raw_data = contenu 
                
+#     execute_js('./difodoo/addons_gesprim/difodoo_fichiers_base/script/di_impression_client.js')
+#     function openWin() {
+#     var printWindow = window.open();
+#     printWindow.document.open('text/plain')
+#     printWindow.document.write('${raw_data}$');
+#     printWindow.document.close();
+#     printWindow.focus();
+#     printWindow.print();
+#   }
 #     printer_name = win32print.GetDefaultPrinter ()  
     hPrinter = win32print.OpenPrinter (printer)
 #    hPrinter = win32print.OpenPrinter (printer_name)
