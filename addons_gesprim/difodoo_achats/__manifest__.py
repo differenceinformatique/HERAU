45710f5fe023c37d62ad717a639b7289bb8793b3
Steven CASTEL <steven.castel@groupeadinfo.com>
2018-08-02 15:47:06 +0200

    import stock validation apport

--- addons_gesprim/difodoo_achats/__manifest__.py
+++ addons_gesprim/difodoo_achats/__manifest__.py
@@ -25,6 +25,7 @@
     'data': [        
         "views/di_inh_purchase_view.xml",
         "views/di_apportprod_view.xml",
+        "wizard/di_valider_apport_wizard.xml",
         "report/di_purchase_quotation_templates.xml",
         "report/di_purchase_order_templates.xml"
 #         "views/di_inherited_picking_view.xml",
