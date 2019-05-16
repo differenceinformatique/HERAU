741d87f13b125eafe55a2aa9c675e9ca22703fb3
Steven CASTEL <Steven.Castel@groupeadinfo.com>
2019-05-16 14:42:14 +0200

    date fact avant valid

--- addons_gesprim/difodoo_ventes/wizard/di_facturation_cron_wiz.py
+++ addons_gesprim/difodoo_ventes/wizard/di_facturation_cron_wiz.py
@@ -72,10 +72,10 @@ class DiFactCronWiz(models.TransientModel):
             s_o.action_done()
              
         invoices = sale_orders.mapped('invoice_ids')
+        invoices.write({'date_invoice':date_fact})
         param = self.env['di.param'].search([('di_company_id','=',self.env.user.company_id.id)])
         if param.di_autovalid_fact_ven:
-                invoices.action_invoice_open()
-        invoices.write({'date_invoice':date_fact})
+                invoices.action_invoice_open()        
         if not di_avec_fact:    
             if invoices:
                 di_avec_fact = True  
